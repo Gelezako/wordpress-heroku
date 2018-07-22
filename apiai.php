a52428d074fdcb751f0952e968ddeafb983db1e3
Alex Sokolov <Gelezako@users.noreply.github.com>
2018-07-22 18:34:28 +0300

    Update apiai.php

--- apiai.php
+++ apiai.php
@@ -1,30 +1,31 @@
-такого 
-/*
- * PROCESSANDO A MENSAGEM 
- * QUE CHEGA DO BOT
- */
+<?php
+//OG Code by: Bruno_Gastaldi @ https://discuss.api.ai/t/webhook-in-php-example/229/8
+//Modified by: Kevin.
+
+header('Content-Type: application/json');
 function processMessage($update) {
-    if($update["result"]["action"] == "buscar.nfe"){
+    if($update["result"]["action"] == ""){
+    	//Create File.
+	$fp = file_put_contents( 'request.log', $update["result"]["parameters"]["msg"] );
+		//Return response.
         sendMessage(array(
             "source" => $update["result"]["source"],
-            "speech" => "my test",
-            "displayText" => "my anv",
+            "speech" => $update["result"]["parameters"]["msg"],
+            "displayText" => ".........TEXT HERE...........",
             "contextOut" => array()
         ));
     }
 }
-/*
- * FUNÇÃO PARA ENVIAR A MENSAGEM
- */
 function sendMessage($parameters) {
+	$req_dump = print_r( $parameters, true );
+	$fp = file_put_contents( 'reques4.log', $req_dump );
+	header('Content-Type: application/json');
     echo json_encode($parameters);
 }
 
-/*
- * PEGANDO A REQUISIÇÃO
- */
 $update_response = file_get_contents("php://input");
 $update = json_decode($update_response, true);
 if (isset($update["result"]["action"])) {
     processMessage($update);
 }
+?>
